6df32d3ad8bf0531a9de17201907499d1aa50aad
Yash Lathigara <52396302+YashLathigara@users.noreply.github.com>
2020-05-30 21:12:27 +0530

    Update rockpaperscissors.js

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -10,17 +10,16 @@ return userInput;
 
 const getComputerChoice = computerInput => {
   const choice = Math.floor(Math.random()* 3 );
-  //return choice;
   switch (choice){
   case 0:
   return 'rock';
-  //break;
+  break;
   case 1:
   return 'paper';
-  //break;
+  break;
   case 2:
   return 'scissors';
-  //break;
+  break;
   }
 }
 
@@ -30,13 +29,13 @@ if (userChoice === computerChoice) {
 } else if(userChoice === 'rock'&& computerChoice === 'paper') {
   return 'pc wins'
 } else if(userChoice === 'rock'&& computerChoice === 'scissors') {
-  return 'saroj wins'
+  return 'user wins'
 } else if(userChoice === 'paper'&& computerChoice === 'rock') {
-  return 'saroj wins'
+  return 'user wins'
 } else if(userChoice === 'paper'&& computerChoice === 'scissors') {
   return 'pc wins'
 } else if(userChoice === 'scissors'&& computerChoice === 'paper') {
-  return 'saroj wins'
+  return 'user wins'
 } else if(userChoice === 'scissors'&& computerChoice === 'rock') {
   return 'pc wins'
 }
@@ -45,7 +44,7 @@ if (userChoice === computerChoice) {
 function playGame() {
   const userChoice = getUserChoice('paper');
   const computerChoice = getComputerChoice();
-  console.log('saroj threw: ' + userChoice);
+  console.log('user threw: ' + userChoice);
   console.log('The computer threw: ' + computerChoice);
   console.log(determineWinner(userChoice, computerChoice));
 }
